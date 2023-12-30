f3747512f23d72bf9e8e7c304da4e85ba9c84638
absks865 <155258370+absks865@users.noreply.github.com>
2023-12-31 00:21:47 +0500

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,6 @@
-// import { Controller, Get  } from '@nestjs/common';
-// import { AppService } from './app.service';
-
-// @Controller('message')
-// export class AppController {
-//   constructor(private readonly appService: AppService) {}
-
-//   @Get()
-//   getHello(): string {
-//     return this.appService.getHello();
-//   }
-// }
-// app.controller.ts
-
 import { Controller, Get } from '@nestjs/common';
 
-@Controller()
+@Controller('api') // <- Add a route prefix
 export class AppController {
   @Get()
   getHello(): string {
